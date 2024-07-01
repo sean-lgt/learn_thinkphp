a87e0d1279b7141c535c4ccf2040b00bad7b0f8d
林桂涛 <852951042@qq.com>
2024-07-01 11:30:56 +0800

    fix: 修复安装路由注解报错，composer require topthink/think-annotation 1.1

---
+++ tp-first-start/app/controller/DependInject.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\controller;
+
+use app\BaseController;
+
+
+
+class DependInject extends BaseController
+{
+  public function index()
+  {
+    return 'hello inject';
+  }
+}
